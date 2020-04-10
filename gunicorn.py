9acea121e59f081416250e3d1c49d96e1e273248
Angelo Yang <angelo.r.yang@gmail.com>
2020-04-10 13:40:39 -0500

    yes

--- gunicorn.py
+++ gunicorn.py
@@ -3,7 +3,7 @@ import os
 
 host = '[::1]'
 port = 5000
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 if 'PORT' in os.environ:
     host = '0.0.0.0'
     port = int(os.environ['PORT'])
